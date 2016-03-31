fe169e3b13930246749335b3e312e521d15b1e8f
ql91 <448325355@qq.com>
2016-03-31 10:13:49 +0800

    Site updated: 2016-03-31 10:13:39

--- js/main.js
+++ js/main.js
@@ -1,110 +1,179 @@
-require([], function (){
+(function(w, d) {
 
-	var isMobileInit = false;
-	var loadMobile = function(){
-		require(['/js/mobile.js'], function(mobile){
-			mobile.init();
-			isMobileInit = true;
-		});
-	}
-	var isPCInit = false;
-	var loadPC = function(){
-		require(['/js/pc.js'], function(pc){
-			pc.init();
-			isPCInit = true;
-		});
-	}
+    var body = d.body,
+        gotop = d.getElementById('gotop'),
+        menu = d.getElementById('menu'),
+        header = d.getElementById('header'),
+        mask = d.getElementById('mask'),
+        menuToggle = d.getElementById('menu-toggle'),
+        menuOff = d.getElementById('menu-off'),
+        menuShare = d.getElementById('menu-share'),
+        loading = d.getElementById('loading'),
+        animate = w.requestAnimationFrame,
+        ua = navigator.userAgent,
+        isMD = ua.indexOf('Mobile') !== -1 || ua.indexOf('Android') !== -1 || ua.indexOf('iPhone') !== -1 || ua.indexOf('iPad') !== -1 || ua.indexOf('KFAPWI') !== -1,
+        even = isMD ? 'touchstart' : 'click',
+        noop = function() {},
+        offset = function(el) {
+            var x = el.offsetLeft,
+                y = el.offsetTop;
 
-	var browser={
-	    versions:function(){
-	    var u = window.navigator.userAgent;
-	    return {
-	        trident: u.indexOf('Trident') > -1, //IE内核
-	        presto: u.indexOf('Presto') > -1, //opera内核
-	        webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
-	        gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
-	        mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
-	        ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
-	        android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
-	        iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //是否为iPhone或者安卓QQ浏览器
-	        iPad: u.indexOf('iPad') > -1, //是否为iPad
-	        webApp: u.indexOf('Safari') == -1 ,//是否为web应用程序，没有头部与底部
-	        weixin: u.indexOf('MicroMessenger') == -1 //是否为微信浏览器
-	        };
-	    }()
-	}
+            if (el.offsetParent) {
+                var pOfs = arguments.callee(el.offsetParent);
+                x += pOfs.x;
+                y += pOfs.y;
+            }
 
-	$(window).bind("resize", function(){
-		if(isMobileInit && isPCInit){
-			$(window).unbind("resize");
-			return;
-		}
-		var w = $(window).width();
-		if(w >= 700){
-			loadPC();
-		}else{
-			loadMobile();
-		}
-	});
+            return {
+                x: x,
+                y: y
+            };
+        };
 
-	if(browser.versions.mobile === true || $(window).width() < 700){
-		loadMobile();
-	}else{
-		loadPC();
-	}
+    var Blog = {
+        goTop: function() {
+            var top = body.scrollTop;
+            if (top > 400) {
+                body.scrollTop = top - 400;
+                animate(arguments.callee);
+            } else {
+                body.scrollTop = 0;
+            }
+        },
+        toggleGotop: function(top) {
+            if (top > w.innerHeight / 2) {
+                gotop.classList.add('in');
+            } else {
+                gotop.classList.remove('in');
+            }
+        },
+        toggleMenu: function(flag) {
+            if (flag) {
+                menu.classList.remove('hide');
 
-	//是否使用fancybox
-	if(yiliaConfig.fancybox === true){
-		require(['/fancybox/jquery.fancybox.js'], function(pc){
-			var isFancy = $(".isFancy");
-			if(isFancy.length != 0){
-				var imgArr = $(".article-inner img");
-				for(var i=0,len=imgArr.length;i<len;i++){
-					var src = imgArr.eq(i).attr("src");
-					var title = imgArr.eq(i).attr("alt");
-					imgArr.eq(i).replaceWith("<a href='"+src+"' title='"+title+"' rel='fancy-group' class='fancy-ctn fancybox'><img src='"+src+"' title='"+title+"'></a>");
-				}
-				$(".article-inner .fancy-ctn").fancybox();
-			}
-		});
-		
-	}
-	//是否开启动画
-	if(yiliaConfig.animate === true){
+                if(w.innerWidth < 1241) {
+                    mask.classList.add('in');
+                    menu.classList.add('show');
+                }
 
-		require(['/js/jquery.lazyload.js'], function(){
-			//avatar
-			$(".js-avatar").attr("src", $(".js-avatar").attr("lazy-src"));
-			$(".js-avatar")[0].onload = function(){
-				$(".js-avatar").addClass("show");
-			}
-		});
-		
-		if(yiliaConfig.isHome === true){
-			//content
-			function showArticle(){
-				$(".article").each(function(){
-					if( $(this).offset().top <= $(window).scrollTop()+$(window).height() && !($(this).hasClass('show')) ) {
-						$(this).removeClass("hidden").addClass("show");
-						$(this).addClass("is-hiddened");
-					}else{
-						if(!$(this).hasClass("is-hiddened")){
-							$(this).addClass("hidden");
-						}
-					}
-				});
-			}
-			$(window).on('scroll', function(){
-				showArticle();
-			});
-			showArticle();
-		}
-		
-	}
-	
-	//是否新窗口打开链接
-	if(yiliaConfig.open_in_new == true){
-		$(".article a[href]").attr("target", "_blank")
-	}
-	
-});
+            } else {
+                menu.classList.remove('show');
+                mask.classList.remove('in');
+            }
+        },
+        fixedHeader: function(top) {
+            if (top > header.clientHeight) {
+                header.classList.add('fixed');
+            } else {
+                header.classList.remove('fixed');
+            }
+        },
+        fixedToc: (function() {
+            var toc = d.getElementById('post-toc');
+
+            if (!toc) {
+                return noop;
+            }
+
+            var tocOfs = offset(toc),
+                tocTop = tocOfs.y;
+
+            return function(top) {
+                if (top > tocTop) {
+                    toc.classList.add('fixed');
+                } else {
+                    toc.classList.remove('fixed');
+                }
+            };
+        })(),
+        share: function() {
+
+            var share = d.getElementById('global-share'),
+                data = share.dataset,
+                title = data.title,
+                url = data.url,
+                summary = data.summary,
+                pic = data.pic,
+                div = d.createElement('div'),
+                sns = d.getElementsByClassName('share-sns'),
+                api;
+
+            div.innerHTML = summary;
+            summary = div.innerText;
+            div = undefined;
+
+            api = 'http://www.jiathis.com/send/?webid={service}&url=' + url + '&title=' + title + '&summary=' + summary + '&pic=' + pic;
+
+            function goShare(service) {
+                w.open(encodeURI(api.replace('{service}', service)));
+            }
+
+            [].forEach.call(sns, function(el) {
+                el.addEventListener('click', function() {
+                    goShare(this.dataset.service);
+                }, false);
+            });
+
+            return {
+                show: function() {
+                    mask.classList.add('in');
+                    share.classList.add('in');
+                },
+                hide: function() {
+                    share.classList.remove('in');
+                    mask.classList.remove('in');
+                }
+            };
+        }
+    };
+
+    menu.addEventListener('touchmove', function(e) {
+        e.preventDefault();
+    });
+
+
+    w.addEventListener('load', function() {
+        loading.classList.remove('active');
+    });
+
+    w.addEventListener('resize', function() {
+        Blog.toggleMenu();
+    });
+
+    var share = Blog.share();
+
+    menuShare.addEventListener(even, function() {
+        share.show();
+    }, false);
+
+    gotop.addEventListener(even, function() {
+        animate(Blog.goTop);
+    }, false);
+
+    menuToggle.addEventListener(even, function() {
+        Blog.toggleMenu(true);
+    }, false);
+
+    menuOff.addEventListener(even, function() {
+        menu.classList.add('hide');
+    }, false);
+
+    mask.addEventListener(even, function() {
+        Blog.toggleMenu();
+        share.hide();
+    }, false);
+
+    d.addEventListener('scroll', function() {
+        var top = body.scrollTop;
+        Blog.toggleGotop(top);
+        Blog.fixedHeader(top);
+        Blog.fixedToc(top);
+    }, false);
+
+    Waves.init();
+    Waves.attach('.waves-button-light', ['waves-button', 'waves-light']);
+    Waves.attach('.waves-circle-light', ['waves-circle', 'waves-light']);
+    Waves.attach('.waves-block, .global-share li', ['waves-block']);
+    Waves.attach('.article-tag-list-link, #page-nav a, #page-nav span', ['waves-button']);
+
+})(window, document);
